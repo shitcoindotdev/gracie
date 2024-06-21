0b5bb5874baf8288ff778afbeced8847d04dcd07
shitcoindotdev <boomer@shitcoin.dev>
2024-06-21 14:46:41 +0200

    mem

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Marquee from './components/Marquee';
 import gracie from './assets/cracie.webp';
 import FloatingEmojis from './components/FloatingEmojis';
 import gracielogo from './assets/gracielogo.png';
+import pumppill from './assets/logo.webp';
 
 function App() {
   const [isOpen, setIsopen] = useState(true);
@@ -15,13 +16,13 @@ function App() {
     <>
       <FloatingEmojis />
       <div
-        className="relative z-20 w-full overflow-hidden"
+        className="relative z-20 w-full max-w-[1200px] overflow-hidden"
         style={{ fontFamily: 'Roboto, sans-serif' }}
       >
         <div>
           <img
             src={gracielogo}
-            className="h-16 w-auto mx-[5vw] sm"
+            className="h-16 w-auto mx-2 my-2 sm:h-24 sm:mt-4"
             alt="gracie logo"
           />
         </div>
@@ -46,7 +47,7 @@ function App() {
           <div className="w-full flex justify-center gap-4 flex-col items-center my-8 lg:mb-40">
             <div className="flex flex-col items-center gap-2">
               <h1 className="text-3xl font-bold sm:text-[52px]">$Gracie</h1>
-              <h2 className="text-[12px] sm:text-[20px] text-black/75">
+              <h2 className="text-[9px] sm:text-[18px] text-black/75">
                 HE3bra2UhdhBrCX1yzLHFVjFaFp591zSMvYzo3ropump
               </h2>
             </div>
@@ -153,6 +154,47 @@ function App() {
               </div>
             </div>
           </div>
+          <section id="faq" className="flex flex-col gap-4 px-4 py-16">
+            <div className="rounded-xl bg-[#e22e7c] text-white">
+              <div>
+                <details className="group peer">
+                  <summary className="flex cursor-pointer items-center gap-2 px-4 py-2">
+                    <h2 className="text-left font-semibold">
+                      Who is Noland Arbaugh?
+                    </h2>
+                  </summary>
+                </details>
+                <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-[250ms] ease-in-out peer-open:grid-rows-[1fr]">
+                  <div className="overflow-hidden">
+                    <p className="group-open:border-t-1 mx-4 -translate-y-3 border-t border-white py-3 text-left text-rose-50 transition-all duration-[250ms] ease-in-out">
+                      Noland Arbaugh is the first human to receive a Neuralink
+                      brain-computer interface implant. After a 2016 swimming
+                      accident left him quadriplegic, Arbaugh received the
+                      implant in early 2024. The device allows him to control a
+                      computer cursor with his thoughts, enhancing his digital
+                      autonomy despite some initial technical issues​
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="rounded-xl bg-[#e22e7c] text-white">
+              <div>
+                <details className="group peer">
+                  <summary className="flex cursor-pointer items-center gap-2 px-4 py-2">
+                    <h2 className="text-left font-semibold">Who is Gracie?</h2>
+                  </summary>
+                </details>
+                <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-[250ms] ease-in-out peer-open:grid-rows-[1fr]">
+                  <div className="overflow-hidden">
+                    <div className="group-open:border-t-1 mx-4 flex -translate-y-3 flex-col gap-4 border-t border-white py-3 text-left text-rose-50 transition-all duration-[250ms] ease-in-out">
+                      <p>Gracie is Nolands Fluffy Doggo</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </main>
       </div>
       <Marquee imageSrc={banner} altText="neurolution" speed="10" />
