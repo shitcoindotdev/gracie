b545b8949994b3fb0d26cf54399ece656b1e4250
shitcoindotdev <boomer@shitcoin.dev>
2024-08-16 01:30:54 +0200

    md

--- src/components/TweetEmbed.jsx
+++ src/components/TweetEmbed.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { TwitterTweetEmbed } from 'react-twitter-embed'
 
-const App = () => {
+const TweetEmbed = () => {
   return (
     <div className="flex w-full max-w-[600px] items-center justify-center">
       <TwitterTweetEmbed tweetId={'1824208235475198311'} />
@@ -9,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default TweetEmbed
